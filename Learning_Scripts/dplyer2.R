17f7a77130f933b3566af582d85bebfed3e834a6
Pansuriya Foram Rasikbhai <panforam@gmail.com>
2025-03-08 23:58:31 +0530

    Add files via upload

---
+++ Learning_Scripts/dplyer2.R
@@ -0,0 +1,10 @@
+# creating a data frame with stringAsFactors = FALSE
+grades <- data.frame(names = c("John", "Juan", "Jean", "Yao"), 
+                     exam_1 = c(95, 80, 90, 85), 
+                     exam_2 = c(90, 85, 85, 90),
+                     stringsAsFactors = FALSE)
+
+s <- heights |> 
+  filter(sex == "Female") |>
+  summarize(average = mean(height), standard_deviation = sd(height))
+s
